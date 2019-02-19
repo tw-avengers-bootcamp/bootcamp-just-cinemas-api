3c17c3bee60debbc2adcbf6def0ea2b3b352cde0
narendrakumar.b <narendrakumar.b@INNarendrakumarbs-MacBook-Pro.local>
2019-02-19 12:24:29 +0530

    added sql changs

--- src/main/resources/db/migration/V3__create_movie.sql
+++ src/main/resources/db/migration/V3__create_movie.sql
@@ -1,60 +1,81 @@
-DROP TABLE IF EXISTS Status;
+DROP TABLE IF EXISTS Status CASCADE;
 CREATE TABLE IF NOT EXISTS Status (
        status_id serial PRIMARY KEY,
        status VARCHAR(25) CHECK (status IN ('now showing', 'upcoming')) DEFAULT 'upcoming'
 );
 
-DROP TABLE IF EXISTS Location;
+DROP TABLE IF EXISTS Location CASCADE;
 CREATE TABLE IF NOT EXISTS Location (
        location_id serial PRIMARY KEY,
        location VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS Still;
-CREATE TABLE IF NOT EXISTS Still (
-       still_id serial PRIMARY KEY,
-       still_url text
-);
-
-DROP TABLE IF EXISTS Trailer;
-CREATE TABLE IF NOT EXISTS Trailer (
-       trailer_id serial PRIMARY KEY,
-       trailer_url text
-);
-
-DROP TABLE IF EXISTS Experience;
-CREATE TABLE IF NOT EXISTS Experience (
-       experience_id serial PRIMARY KEY,
-       experience text
-);
-
-DROP TABLE IF EXISTS LANGUAGE;
+DROP TABLE IF EXISTS LANGUAGE CASCADE;
 CREATE TABLE IF NOT EXISTS LANGUAGE (
        language_id serial PRIMARY KEY,
        LANGUAGE VARCHAR(25)
 );
-
-DROP TABLE IF EXISTS Banner;
-CREATE TABLE IF NOT EXISTS Banner (
-       banner_id serial PRIMARY KEY,
-       banner_url text
-);
-
-DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Movie CASCADE;
 CREATE TABLE IF NOT EXISTS Movie (
        movie_id serial PRIMARY KEY,
        movie_name VARCHAR(255) NOT NULL,
        synopsis TEXT NOT NULL,
        rating INTEGER,
        status_id INTEGER REFERENCES Status(status_id) ,
-       location_id INTEGER REFERENCES Location(location_id),
        language_id INTEGER REFERENCES language(language_id),
-       banner_id INTEGER REFERENCES Banner(banner_id) ON DELETE CASCADE
+       banner_url TEXT
 );
 
-DROP TABLE IF EXISTS Movie_Location;
+
+
+DROP TABLE IF EXISTS Still CASCADE;
+CREATE TABLE IF NOT EXISTS Still (
+       still_id serial PRIMARY KEY,
+       still_url text,
+       movie_id serial
+);
+
+DROP TABLE IF EXISTS Trailer CASCADE;
+CREATE TABLE IF NOT EXISTS Trailer (
+       trailer_id serial PRIMARY KEY,
+       trailer_url text,
+       movie_id serial
+);
+
+DROP TABLE IF EXISTS Experience CASCADE;
+CREATE TABLE IF NOT EXISTS Experience (
+       experience_id serial PRIMARY KEY,
+       experience text
+);
+
+
+
+DROP TABLE IF EXISTS Banner CASCADE;
+
+
+DROP TABLE IF EXISTS Movie_Location CASCADE;
 CREATE TABLE IF NOT EXISTS Movie_Location (
        movie_location_id serial PRIMARY KEY,
        movie_id INTEGER REFERENCES movie(movie_id),
        location_id INTEGER REFERENCES location(location_id)
 );
+
+---trailer,stills
+
+/*
+DROP TABLE IF EXISTS Status CASCADE;
+DROP TABLE IF EXISTS Location CASCADE;
+DROP TABLE IF EXISTS LANGUAGE CASCADE;
+DROP TABLE IF EXISTS Movie CASCADE;
+DROP TABLE IF EXISTS Still CASCADE;
+DROP TABLE IF EXISTS Trailer CASCADE;
+DROP TABLE IF EXISTS Experience CASCADE;
+DROP TABLE IF EXISTS Banner CASCADE;
+
+
+DROP TABLE IF EXISTS Movie_Location CASCADE;
+DROP TABLE IF EXISTS SCHEMA_VERSION CASCADE;
+DROP TABLE IF EXISTS SCHEMA_VERSION CASCADE;
+*/
+
+
