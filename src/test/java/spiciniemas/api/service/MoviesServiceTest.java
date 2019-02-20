9c23f17b169e3255f7c105573f13ed66abce58cc
dharanikumar <dharani939@gmail.com>
2019-02-20 14:43:04 +0530

    added test Movie detail and Movie service @dharanikumar @narasimha

---
+++ src/test/java/spiciniemas/api/service/MoviesServiceTest.java
@@ -0,0 +1,11 @@
+package spiciniemas.api.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.test.context.ContextConfiguration;
+import spicinemas.api.service.MovieService;
+
+@RunWith(JUnit4.class)
+@ContextConfiguration(classes = MovieService.class)
+public class MovieServiceTest {
+}
