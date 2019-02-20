1a313ba87eea402b9a4a4f14134034d9dec61bdc
dharanikumar <dharani939@gmail.com>
2019-02-20 14:44:47 +0530

    added test Movie detail and Movie service @dharanikumar @narasimha

--- src/main/java/spicinemas/api/service/MovieService.java
+++ src/main/java/spicinemas/api/service/MovieService.java
@@ -1,5 +1,40 @@
 package spicinemas.api.service;
 
-public class MoivieService {
+import org.springframework.stereotype.Service;
+import spicinemas.api.db.MoviesRepository;
+import spicinemas.api.db.entities.LanguageEntity;
+import spicinemas.api.db.entities.MovieEntity;
+import spicinemas.api.model.Experience;
+import spicinemas.api.model.Language;
+import spicinemas.api.model.Location;
+import spicinemas.api.model.Movie;
 
+import java.util.BitSet;
+
+@Service
+public class MovieService {
+
+    private MoviesRepository moviesRepository;
+
+    public MovieService(MoviesRepository moviesRepository) {
+
+        this.moviesRepository = moviesRepository;
+    }
+
+    public Movie getMovieDetails(long movie_id) {
+        MovieEntity movieEntity = moviesRepository.findOne(movie_id);
+        Movie movie = new Movie();
+        movie.setMovieId(movieEntity.getId());
+        movie.setMovieName(movieEntity.getName());
+        movie.setSynopsis(movieEntity.getSynopsis());
+        //LanguageEntity languageEntity = movieEntity.getLanguage();
+        Language language = new Language();
+        language.setLanguage(movieEntity.getLanguage().getName());
+        movie.setLanguage(language);
+        Experience experience = new Experience();
+        experience.setExperienceId(movieEntity.getExperienceEntity().getId());
+        experience.setName(movieEntity.getExperienceEntity().getType());
+        movie.setExperiences(experience);
+        return movie;
+    }
 }
--- src/test/java/spiciniemas/api/service/MoviesServiceTest.java
+++ src/test/java/spiciniemas/api/service/MoviesServiceTest.java
@@ -1,11 +1,59 @@
 package spiciniemas.api.service;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
+import spicinemas.api.db.MoviesRepository;
+import spicinemas.api.db.entities.LanguageEntity;
+import spicinemas.api.db.entities.LocationEntity;
+import spicinemas.api.db.entities.MovieEntity;
+import spicinemas.api.db.entities.StatusEntity;
 import spicinemas.api.service.MovieService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(JUnit4.class)
 @ContextConfiguration(classes = MovieService.class)
-public class MovieServiceTest {
+public class MoviesServiceTest {
+    MoviesRepository moviesRepository;
+
+    @Before
+    public void init() {
+        moviesRepository = Mockito.mock(MoviesRepository.class);
+        Set<LocationEntity> locationEntities = new HashSet<>();
+        LanguageEntity languageEntity = Mockito.mock(LanguageEntity.class);
+        StatusEntity statusEntity = Mockito.mock(StatusEntity.class);
+        LocationEntity locationEntity = new LocationEntity();
+        MovieEntity movieEntity = Mockito.mock(MovieEntity.class);
+        languageEntity.setId(1l);
+        languageEntity.setName("Tamil");
+        statusEntity.setId(1l);
+        statusEntity.setName("now showing");
+        locationEntity.setId(1l);
+        locationEntity.setName("Chennai");
+        locationEntities.add(locationEntity);
+
+        movieEntity.setId(1l);
+        movieEntity.setSynopsis("abcd synopsis");
+        movieEntity.setName("Kabaali");
+        movieEntity.setLanguage(languageEntity);
+        movieEntity.setLocations(locationEntities);
+        movieEntity.setStatus(statusEntity);
+
+        Mockito.when(moviesRepository.findOne(1l)).thenReturn(movieEntity);
+
+    }
+
+    @Test
+    public void testGetMovieDetails() {
+        MovieService movieService = new MovieService(moviesRepository);
+        Assert.assertNotNull(movieService.getMovieDetails(1l));
+
+    }
 }
