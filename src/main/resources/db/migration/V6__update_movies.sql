d7ade6abc1c052429e7b7ec5085cac734db534e1
dharanikumar <dharani939@gmail.com>
2019-02-21 16:32:13 +0530

    Add migration scripts for more movie data @dharanikumar @nimalan

---
+++ src/main/resources/db/migration/V6__update_movies.sql
@@ -0,0 +1,83 @@
+
+update Movie  set banner_url='https://img.spicinemas.in/resources/images/movies/kabali/1000x320.jpg' where banner_url='https://img.spicinemas.in/resources/images/movies/Kabali/1000x320.jpg';
+update Movie  set banner_url='https://img.spicinemas.in/resources/images/movies/banjo/1000x320.jpg' where banner_url='https://img.spicinemas.in/resources/images/movies/Banjo/1000x320.jpg';
+update Movie  set banner_url='https://img.spicinemas.in/resources/images/movies/suicide/1000x320.jpg' where banner_url='https://img.spicinemas.in/resources/images/movies/Suicide/1000x320.jpg';
+update Movie  set banner_url='https://img.spicinemas.in/resources/images/movies/namadhu/1000x320.jpg' where banner_url='https://img.spicinemas.in/resources/images/movies/Namadhu/1000x320.jpg';
+update Movie  set banner_url='https://img.spicinemas.in/resources/images/movies/pink/1000x320.jpg' where banner_url='https://img.spicinemas.in/resources/images/movies/Pink/1000x320.jpg';
+
+
+update Still  set url='https://img.spicinemas.in/resources/images/movies/kabali/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Kabali/150x207.jpg';
+update Still  set url='https://img.spicinemas.in/resources/images/movies/banjo/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Banjo/150x207.jpg';
+update Still  set url='https://img.spicinemas.in/resources/images/movies/suicide/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Suicide/150x207.jpg';
+update Still  set url='https://img.spicinemas.in/resources/images/movies/pelli/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Pelli/150x207.jpg';
+update Still  set url='https://img.spicinemas.in/resources/images/movies/pink/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Pink/150x207.jpg';
+update Still  set url='https://img.spicinemas.in/resources/images/movies/remo/150x207.jpg' where url='https://img.spicinemas.in/resources/images/movies/Remo/150x207.jpg';
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Kabali2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 1, 3, 'https://img.spicinemas.in/resources/images/movies/kabali/1000x320.jpg');
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Sultan2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 1, 1, 'https://img.spicinemas.in/resources/images/movies/sultan/1000x320.jpg');
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Banjo2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 1, 1, 'https://img.spicinemas.in/resources/images/movies/banjo/1000x320.jpg');
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Suicide2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 1, 1, 'https://img.spicinemas.in/resources/images/movies/suicide/1000x320.jpg');
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Namadhu2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 2, 1, 'https://img.spicinemas.in/resources/images/movies/namadhu/1000x320.jpg');
+
+INSERT INTO Movie(name, synopsis, rating, status_id, language_id, banner_url) VALUES('Pink2', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, feugiat sit amet sagittis nec, viverra vehicula orci. Sed pulvinar imperdiet nunc vel fringilla. In ac facilisis orci. Ut suscipit nisl scelerisque elit finibus, sed auctor velit placerat. Mauris et lacus in felis finibus dictum vel non mauris. Integer feugiat augue vitae mauris ultricies sodales. Nam semper tincidunt viverra. Aliquam pellentesque dolor nec tortor semper, sed rhoncus magna tincidunt.', 5, 1, 2, 'https://img.spicinemas.in/resources/images/movies/pink/1000x320.jpg');
+
+
+--------
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Kabali2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Kabali2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Kabali2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Kabali2';
+--
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Sultan2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Sultan2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Sultan2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Sultan2';
+--
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Banjo2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Banjo2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Banjo2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Banjo2';
+
+--
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Suicide2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Suicide2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Suicide2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Suicide2';
+--
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Namadhu2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Namadhu2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Namadhu2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Namadhu2';
+--
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 1 from Movie where name = 'Pink2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 2 from Movie where name = 'Pink2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 3 from Movie where name = 'Pink2';
+INSERT INTO Movie_Location(movie_id, location_id) select Movie.id, 4 from Movie where name = 'Pink2';
+
+
+INSERT INTO Experience(id, type,movie_id) select 7,'RDX, Dolby Atmos, SUB',Movie.id from Movie where name = 'Kabali2';
+INSERT INTO Experience(id, type,movie_id) select 8,'RDX 3D',Movie.id from Movie where name = 'Sultan2';
+INSERT INTO Experience(id, type,movie_id) select 9,'RDX, SUB',Movie.id from Movie where name = 'Banjo2';
+INSERT INTO Experience(id, type,movie_id) select 10,'RDX, SUB',Movie.id from Movie where name = 'Suicide2';
+INSERT INTO Experience(id, type,movie_id) select 11,'RDX, SUB',Movie.id from Movie where name = 'Namadhu2';
+INSERT INTO Experience(id, type,movie_id) select 12,'RDX, SUB',Movie.id from Movie where name = 'Pink2';
+
+INSERT INTO Still(id, url, movie_id) select 7,'https://img.spicinemas.in/resources/images/movies/kabali/150x207.jpg',Movie.id from Movie where name = 'Kabali2';
+INSERT INTO Still(id, url, movie_id) select 8,'https://img.spicinemas.in/resources/images/movies/banjo/150x207.jpg',Movie.id from Movie where name = 'Sultan2';
+INSERT INTO Still(id, url, movie_id) select 9,'https://img.spicinemas.in/resources/images/movies/suicide/150x207.jpg',Movie.id from Movie where name = 'Banjo2';
+INSERT INTO Still(id, url, movie_id) select 10,'https://img.spicinemas.in/resources/images/movies/kabali/150x207.jpg',Movie.id from Movie where name = 'Suicide2';
+INSERT INTO Still(id, url, movie_id) select 11,'https://img.spicinemas.in/resources/images/movies/pink/150x207.jpg',Movie.id from Movie where name = 'Namadhu2';
+INSERT INTO Still(id, url, movie_id) select 12,'https://img.spicinemas.in/resources/images/movies/remo/150x207.jpg',Movie.id from Movie where name = 'Pink2';
+
+
+INSERT INTO Trailer(id, url, movie_id) select 7,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Kabali2';
+INSERT INTO Trailer(id, url, movie_id) select 8,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Sultan2';
+INSERT INTO Trailer(id, url, movie_id) select 9,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Banjo2';
+INSERT INTO Trailer(id, url, movie_id) select 10,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Suicide2';
+INSERT INTO Trailer(id, url, movie_id) select 11,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Namadhu2';
+INSERT INTO Trailer(id, url, movie_id) select 12,'https://www.youtube.com/watch?v=JfbxcD6biOk',Movie.id from Movie where name = 'Pink2';
+
